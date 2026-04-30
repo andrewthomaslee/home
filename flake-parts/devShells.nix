13f870c8f0a8247c04e603d8e50e282d11390225
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-04-30 09:13:33 -0500

    github pat

--- flake-parts/devShells.nix
+++ flake-parts/devShells.nix
@@ -4,38 +4,37 @@
     lib,
     inputs',
     ...
-  }: let
-    # ------ Common Configuration ------ #
-    packages =
-      [
-        inputs'.clan-core.packages.clan-cli
-        inputs'.kubefetch.packages.default
-        pkgs.k3s
-      ]
-      ++ (with pkgs.unstable; [
-        bash
-        bun
-        fluxcd
-        flux9s
-        cilium-cli
-        kubernetes-helm
-      ]);
-    shellHook = ''
-      eval "$(bunx varlock load --format shell)"
-      export REPO_ROOT
-      REPO_ROOT=$(git rev-parse --show-toplevel)
-      export CLAN_DIR
-      CLAN_DIR=$REPO_ROOT
-      export KUBECONFIG
-      KUBECONFIG=$(find "$REPO_ROOT/.secrets/kubeconfig" -type f 2>/dev/null | paste -sd ":" -)
-
-      kubectl config get-contexts
-    '';
-  in {
-    # ------ Pure Dev Shell ------ #
-    # Activate: `nix develop .#default`
+  }: {
+    # ------ Default Dev Shell ------ #
+    # Activate: `nix develop`
     devShells.default = pkgs.mkShell {
-      inherit shellHook packages;
+      packages =
+        [
+          inputs'.clan-core.packages.clan-cli
+          inputs'.kubefetch.packages.default
+          pkgs.k3s
+        ]
+        ++ (with pkgs.unstable; [
+          bash
+          bun
+          fluxcd
+          flux9s
+          cilium-cli
+          kubernetes-helm
+          kubeseal
+          cloudflared
+        ]);
+      shellHook = ''
+        eval "$(bunx varlock load --format shell)"
+        export REPO_ROOT
+        REPO_ROOT=$(git rev-parse --show-toplevel)
+        export CLAN_DIR
+        CLAN_DIR=$REPO_ROOT
+        export KUBECONFIG
+        KUBECONFIG=$(find "$REPO_ROOT/.secrets/kubeconfig" -type f 2>/dev/null | paste -sd ":" -)
+
+        kubectl config get-contexts
+      '';
     };
   };
 }
