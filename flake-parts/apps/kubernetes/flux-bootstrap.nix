33bc226c382ba04eb11016915a8bd2a86d04af96
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-04-30 12:34:08 -0500

    flux bootstap

---
+++ flake-parts/apps/kubernetes/flux-bootstrap.nix
@@ -0,0 +1,33 @@
+{...}: {
+  # ------ Per-System ------ #
+  perSystem = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    apps = {
+      flux-bootstrap = {
+        type = "app";
+        program = lib.getExe (pkgs.writeShellApplication {
+          name = "flux-bootstrap";
+          runtimeInputs = [
+            pkgs.k3s
+            pkgs.unstable.fluxcd
+          ];
+          text = ''
+            CLUSTER=$1
+            kubectl config use-context "$CLUSTER"
+            flux bootstrap github \
+              --token-auth \
+              --owner=andrewthomaslee \
+              --repository=home \
+              --branch=main \
+              --path=kubernetes/manifests/"$CLUSTER" \
+              --path=kubernetes/manifests/shared \
+              --personal
+          '';
+        });
+      };
+    };
+  };
+}
