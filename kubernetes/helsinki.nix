bcd7ef859861c0200460e2e5d4290fb53d84f273
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-05-01 08:14:18 -0500

    cloudflare tunnels

--- kubernetes/helsinki.nix
+++ kubernetes/helsinki.nix
@@ -16,6 +16,61 @@ in {
     version = kubeVersion;
     namespace = "default";
     helm.releases = {};
-    objects = [];
+    objects = [
+      {
+        apiVersion = "helm.toolkit.fluxcd.io/v2";
+        kind = "HelmRelease";
+        metadata = {
+          name = "cloudflared";
+          namespace = "cloudflare";
+        };
+        spec = {
+          interval = "24h";
+          chart = {
+            spec = {
+              chart = "cloudflared";
+              version = "2.2.7";
+              interval = "24h";
+              sourceRef = {
+                kind = "HelmRepository";
+                name = "cloudflared";
+                namespace = "flux-system";
+              };
+            };
+          };
+          values = {
+            tunnelSecrets = {
+              existingPemFileSecret.name = "cloudflared-cert-pem-file-secret";
+              existingConfigJsonFileSecret.name = "cloudflared-config-json-file-secret";
+            };
+            tunnelConfig.name = "cloud";
+            resources = {
+              limits = {
+                cpu = "200m";
+                memory = "128Mi";
+              };
+              requests = {
+                cpu = "100m";
+                memory = "64Mi";
+              };
+            };
+            # cloudflared tunnel route dns home <domain>.andrewlee.cloud
+            ingress = [
+              {
+                hostname = "whoami.andrewlee.cloud";
+                service = "http://whoami.whoami.svc.cluster.local:80";
+              }
+              {
+                hostname = "hubble.andrewlee.cloud";
+                service = "http://hubble-ui.kube-system.svc.cluster.local:80";
+              }
+              {
+                service = "http_status:404"; # MUST GO LAST
+              }
+            ];
+          };
+        };
+      }
+    ];
   };
 }
