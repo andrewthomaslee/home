a8e5f618dc0ac65699876a3996d08eb68e882f22
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-06-05 08:28:54 -0500

    rancher

--- flake-parts/nixosModules/rancher.nix
+++ flake-parts/nixosModules/rancher.nix
@@ -4,7 +4,7 @@
     cfg = config.hostSpec.services.rancher;
   in {
     options.hostSpec.services.rancher.enable = lib.mkEnableOption "default rancher configuration";
-    config = lib.mkIf cfg.enable {
+    config = lib.mkIf (cfg.enable && config.services.k3s.role == "server") {
       services.k3s.disable = ["local-storage"];
     };
   };
