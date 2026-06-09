757c78de2e9bd72edb5ae01c3d482cc205bb5865
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-06-08 21:58:13 -0500

    inuc-i5 nvme

--- machines/inuc-i5/disko.nix
+++ machines/inuc-i5/disko.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/nvme0n1";
+        device = "/dev/disk/by-id/nvme-SAMSUNG_MZALQ512HALU-000L2_S4UKNF1NC91205";
         content = {
           type = "gpt";
           partitions = {
@@ -30,47 +30,48 @@
                 format = "ext4";
                 mountpoint = "/";
                 mountOptions = ["noatime"];
+                extraArgs = ["-L" "nixos"];
               };
             };
           };
         };
       };
-      sda = {
-        type = "disk";
-        device = "/dev/disk/by-id/ata-ST1000DM003-1SB102_Z9APETJA";
-        content = {
-          type = "gpt";
-          partitions = {
-            data = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/mnt/sda";
-                mountOptions = ["noatime" "nofail"];
-              };
-            };
-          };
-        };
-      };
-      sdb = {
-        type = "disk";
-        device = "/dev/disk/by-id/ata-ST2000DX001-1NS164_Z4Z5RFRN";
-        content = {
-          type = "gpt";
-          partitions = {
-            data = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/mnt/sdb";
-                mountOptions = ["noatime" "nofail"];
-              };
-            };
-          };
-        };
-      };
+      # sda = {
+      #   type = "disk";
+      #   device = "/dev/disk/by-id/ata-ST1000DM003-1SB102_Z9APETJA";
+      #   content = {
+      #     type = "gpt";
+      #     partitions = {
+      #       data = {
+      #         size = "100%";
+      #         content = {
+      #           type = "filesystem";
+      #           format = "ext4";
+      #           mountpoint = "/mnt/sda";
+      #           mountOptions = ["noatime" "nofail"];
+      #         };
+      #       };
+      #     };
+      #   };
+      # };
+      # sdb = {
+      #   type = "disk";
+      #   device = "/dev/disk/by-id/ata-ST2000DX001-1NS164_Z4Z5RFRN";
+      #   content = {
+      #     type = "gpt";
+      #     partitions = {
+      #       data = {
+      #         size = "100%";
+      #         content = {
+      #           type = "filesystem";
+      #           format = "ext4";
+      #           mountpoint = "/mnt/sdb";
+      #           mountOptions = ["noatime" "nofail"];
+      #         };
+      #       };
+      #     };
+      #   };
+      # };
     };
   };
 }
