bb9c809e46f2886309d355faeef060c4a2db6d67
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-06-13 18:45:36 -0500

    icon and plasma

--- flake-parts/homeModules/plasma-manager.nix
+++ flake-parts/homeModules/plasma-manager.nix
@@ -13,20 +13,25 @@
       home.packages = with pkgs; [
         bibata-cursors
         bibata-cursors-translucent
+        papirus-icon-theme
+        tela-circle-icon-theme
+        colloid-icon-theme
+        klassy
+        darkly
       ];
 
-      programs.plasma = lib.optionalAttrs osConfig.services.desktopManager.plasma6.enable {
-        enable = true;
-        workspace = {
-          lookAndFeel = "org.kde.breezedark.desktop";
-          cursor.theme = "Bibata-Modern-Ice";
-        };
-        hotkeys.commands."launch-kalk" = {
-          name = "Launch Konsole";
-          key = "Meta+Alt+K";
-          command = "kalk";
-        };
-      };
+      # programs.plasma = lib.optionalAttrs osConfig.services.desktopManager.plasma6.enable {
+      #   enable = true;
+      #   workspace = {
+      #     lookAndFeel = "org.kde.breezedark.desktop";
+      #     cursor.theme = "Bibata-Modern-Ice";
+      #   };
+      #   hotkeys.commands."launch-kalk" = {
+      #     name = "Launch Konsole";
+      #     key = "Meta+Alt+K";
+      #     command = "kalk";
+      #   };
+      # };
     };
   };
 }
