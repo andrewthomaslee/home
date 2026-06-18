c97cc5899bf3fb385f64b469d3bafd651afd0b5b
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-06-18 16:19:01 -0500

    machines/nixos: set disk schema to btrfs-single-disk-subvolumes-impermanance-tmpfs

--- machines/nixos/disko.nix
+++ machines/nixos/disko.nix
@@ -1,36 +1,124 @@
+# ---
+# schema = "btrfs-single-disk-subvolumes-impermanance-tmpfs"
+# [placeholders]
+# mainDisk = "/dev/disk/by-id/nvme-Patriot_M.2_P300_1024GB_P300ZCCB250907339" 
+# ---
+# This file was automatically generated!
+# CHANGING this configuration requires wiping and reinstalling the machine
 {
-  disko.devices.disk.main = {
-    device = "/dev/disk/by-id/nvme-Patriot_M.2_P300_1024GB_P300ZCCB250907339";
-    type = "disk";
-    content = {
-      type = "gpt";
-      partitions = {
-        boot = {
-          size = "1M";
-          type = "EF02";
-          priority = 1;
-        };
-        ESP = {
-          type = "EF00";
-          size = "5G";
-          priority = 2;
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-            mountOptions = ["umask=0077"];
+  boot.loader.grub = {
+    efiInstallAsRemovable = true;
+    efiSupport = true;
+  };
+
+  disko.devices = {
+    disk = {
+      "main" = {
+        name = "main-4ae019a72251435589518d79410139e3";
+        device = "/dev/disk/by-id/nvme-Patriot_M.2_P300_1024GB_P300ZCCB250907339";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            "boot" = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              priority = 1;
+            };
+            "ESP" = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            "swap" = {
+              size = "8G"; # adjust
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+              };
+            };
+            "root" = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [
+                  "--force"
+                  "--label root"
+                ];
+                subvolumes = {
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [ "compress=zstd" ];
+                  };
+                  "@persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = [ "compress=zstd" ];
+                  };
+                };
+              };
+            };
           };
         };
-        nixos = {
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
-            mountOptions = ["noatime"];
-            extraArgs = ["-L" "nixos"];
-          };
-        };
+      };
+    };
+
+    nodev = {
+      "/" = {
+        fsType = "tmpfs";
+        mountOptions = [
+          "size=3G"
+          "mode=0755"
+          "noexec"
+        ];
+      };
+    };
+  };
+
+  fileSystems."/persist".neededForBoot = true;
+
+  # Automatic local snapshots
+  # https://digint.ch/btrbk/doc/readme.html
+  #$ systemctl start btrbk-<instance>
+  services.btrbk = {
+    instances."nix" = {
+      onCalendar = "0/2:00";
+      settings = {
+        subvolume = "/nix";
+        snapshot_create = "onchange";
+        snapshot_dir = "/nix";
+        snapshot_preserve = "16h 7d 2w";
+        snapshot_preserve_min = "3d";
+      };
+    };
+    instances."home" = {
+      onCalendar = "0/2:00";
+      settings = {
+        subvolume = "/home";
+        snapshot_create = "onchange";
+        snapshot_dir = "/home";
+        snapshot_preserve = "16h 7d 3w 2m";
+        snapshot_preserve_min = "3d";
+      };
+    };
+    instances."persist" = {
+      onCalendar = "0/2:00";
+      settings = {
+        subvolume = "/persist";
+        snapshot_dir = "/persist";
+        snapshot_preserve = "16h 7d 3w 2m";
+        snapshot_preserve_min = "3d";
       };
     };
   };
