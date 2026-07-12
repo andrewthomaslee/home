a36a1812ba05ae85bcc543ae64146f1141c44f91
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-07-12 14:06:51 -0500

    kde

--- inventory.nix
+++ inventory.nix
@@ -49,7 +49,7 @@ in {
     # Hetzner
     hmetal = {
       deploy.targetHost = "root@[2a01:4f9:2a:b8d::2]";
-      tags = ["m" "master" "netsa"];
+      tags = ["m" "master" "kde" "netsa"];
     };
   };
 
