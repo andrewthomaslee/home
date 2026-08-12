dc673ac4a4ed4587219b7b5280082d5434fe41c8
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-08-11 19:56:59 -0500

    remove snapshots

--- machines/nixos/disko.nix
+++ machines/nixos/disko.nix
@@ -54,21 +54,21 @@
                   "@root" = {
                     mountpoint = "/";
                     mountOptions = [
-                      "compress=zstd"
+                      "compress=zstd:3"
                       "noatime"
                     ];
                   };
                   "@nix" = {
                     mountpoint = "/nix";
                     mountOptions = [
-                      "compress=zstd"
+                      "compress=zstd:3"
                       "noatime"
                     ];
                   };
                   "@home" = {
                     mountpoint = "/home";
                     mountOptions = [
-                      "compress=zstd"
+                      "compress=zstd:3"
                       "noatime"
                     ];
                   };
@@ -129,31 +129,6 @@
             };
           };
         };
-      };
-    };
-  };
-
-  # Automatic local snapshots
-  # https://digint.ch/btrbk/doc/readme.html
-  #$ systemctl start btrbk-<instance>
-  services.btrbk = {
-    instances."nix" = {
-      onCalendar = "0/2:00";
-      settings = {
-        subvolume = "/nix";
-        snapshot_create = "onchange";
-        snapshot_dir = "/nix";
-        snapshot_preserve = "16h 7d 2w";
-        snapshot_preserve_min = "3d";
-      };
-    };
-    instances."home" = {
-      onCalendar = "0/2:00";
-      settings = {
-        subvolume = "/home";
-        snapshot_dir = "/home";
-        snapshot_preserve = "16h 7d 3w 2m";
-        snapshot_preserve_min = "3d";
       };
     };
   };
